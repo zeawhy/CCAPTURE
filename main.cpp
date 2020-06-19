21c0cf2b34f7e571fed4246c9a80764564d68620
liuzhidong <adfasf>
2020-06-19 15:24:39 +0800

    新增main.cpp main.h

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zeawhy on 2020/6/19.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by zeawhy on 2020/6/19.
+//
+
+#ifndef CCAPTURE_MAIN_H
+#define CCAPTURE_MAIN_H
+
+#endif //CCAPTURE_MAIN_H
